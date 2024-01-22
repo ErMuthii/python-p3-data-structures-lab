119caa5ad5af740b7046277b0f3a3fb199ac32de
ErMuthii <er.muthii@gmail.com>
2024-01-22 22:17:01 +0300

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,53 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    food_names = [food['name'] for food in spicy_foods]
+    return food_names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest = [food for food in spicy_foods if food.get('spiciness',0) > 5]
+    return spiciest
 
 def print_spicy_foods(spicy_foods):
-    pass
+     for food in spicy_foods:
+        name = food.get('name','Invalid')
+        cuisine = food.get('cuisine','Invalid')
+        heat_level = food.get('heat_level',0)
+
+        emojis = '🌶' * heat_level 
+        print(f'{name} ({cuisine}) | Heat Level: {emojis}')
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    sort_cuisines = [food for food in spicy_foods if food.get('cuisine','Invalid') == cuisine]
+    #This function will not return a single dictionary but a list of all matching dictionaries. if not indexed to pich the first
+    return sort_cuisines[0]
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food.get('name','Invalid')
+        cuisine = food.get('cuisine','Invalid')
+        heat_level =  food.get('heat_level',0) 
+        
+        if heat_level > 5:
+            emojis = '🌶' * heat_level 
+            print(f'{name} ({cuisine}) | Heat Level: {emojis}')
 
 def get_average_heat_level(spicy_foods):
-    pass
+   heat_level = [food.get('heat_level',0) for food in spicy_foods ]
+   count = len(heat_level)
+
+   if count == 0:
+        return 0
+   else:
+       return sum(heat_level) // count
+       
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_food = {
+        'name': spicy_food.get('name',''),
+        'cuisine': spicy_food.get('cuisine',''),
+        'heat_level': spicy_food.get('heat_level',0)
+    }
+
+    spicy_foods.append(spicy_food)
+    return spicy_foods
